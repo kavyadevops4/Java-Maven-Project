2d3f4d7f0a6f2893e6ea0c86df80ed0bfda99aea
kavyadevops4 <smvkavya@gmail.com>
2026-03-11 08:57:25 +0530

    Create AppTest.java

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        System.out.println("Test successful");
+    }
+}
