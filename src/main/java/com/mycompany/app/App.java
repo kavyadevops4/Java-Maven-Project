7033c575b9ec2830e2436640ce1ea87cc555c4fc
kavyadevops4 <smvkavya@gmail.com>
2026-03-11 09:15:08 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,12 @@
 package com.mycompany.app;
 
 public class App {
+
+    public String sayHello() {
+        return "Hello from Maven Project!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello from Maven Project!");
+        System.out.println(new App().sayHello());
     }
 }
